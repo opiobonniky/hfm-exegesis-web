df117833edf84a94082ef97ef4de9d29cecdd205
boniface <2100808160@std.kyu.ac.ug>
2026-09-02 09:45:37 +0300

    working well with no error for now

--- src/features/NotificationSettings/pages/NotificationSettings.tsx
+++ src/features/NotificationSettings/pages/NotificationSettings.tsx
@@ -7,6 +7,7 @@ import {
   NotificationToggle,
   NotificationTimePicker,
   NotificationCardContent,
+  NotificationSettingsLoading,
 } from "../components";
 
 export default function NotificationSettings() {
